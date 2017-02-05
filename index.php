239775eb776fd0b407b2aa28eba5dd4383be77b0
tioguil <guil_stay@hotmail.com>
2017-02-05 19:09:27 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Controlrent</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script type="text/javascript" src="script/jquery-3.1.1.min.js"> </script>
+
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div id="centro">
+        <h3> Bem vindo ao Controlrent</h3>
+        <div id="entrar"> 
+            <div id="entrarForm"> 
+                <form>
+                
+                   Login: <input type="text" name="login" class="caixaTexto caixaTexto1"> <br><br>
+                    Senha: <input type="password" name="senha" class="caixaTexto caixaTexto1"> <br><br>
+                    <input type="submit" name="recuperar" class="button button1" value="Recuperar senha">
+                    <input type="submit" name="entrar" class="button button1" value="Entrar"><br><br>
+
+                </form> 
+            </div>
+        
+        </div>
+      </div>  
+    </body>
+</html>
